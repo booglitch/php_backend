d9271e4ac63e54007c6fc4390a22daefcb04ef8c
booglitch <rhrhkddn22@gmail.com>
2023-09-25 14:49:38 +0900

    modified security function(SQL Injection, XSS)

--- process_create.php
+++ process_create.php
@@ -1,6 +1,13 @@
 <?php
 $conn = mysqli_connect('localhost', 'root', '1234', 'opentutorials');
 
+$filtered = array(
+	'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+	'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+
+);
+
+
 $sql = "INSERT INTO topic (title, description, created) VALUES('{$_POST['title']}','{$_POST['description']}',NOW())";
 
 $result = mysqli_query($conn, $sql);
